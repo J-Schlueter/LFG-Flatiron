0fbe65ec379da219b46382a3fa654e8d5624071f
Kenneth Kim <kkm2059@gmail.com>
2022-01-26 15:23:46 -0500

    Loaded Event Entries on Profile

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,28 @@ import Login from "./components/Login";
 function App() {
   const [currentUser, setCurrentUser] = useState(undefined)
   const [allUsers, setAllUsers] = useState([])
+  const [allEntries, setAllEntries] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3000/users")
     .then(resp => resp.json())
     .then(users => setAllUsers(users))
   },[])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/event_entries")
+    .then(resp => resp.json())
+    .then(entries => setAllEntries(entries))
+},[])
   return (
     <div className="App">
+      <button onClick={() => setCurrentUser(allUsers[0])}>Login as Joshua</button>
+      <button onClick={() => setCurrentUser(allUsers[1])}>Login as Kenneth</button>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage currentUser={currentUser}/>} />
         <Route path="/createevent" element={<CreateEvent />} />
-        <Route path= "/profile" element={<Profile />} />
+        <Route path= "/profile" element={<Profile currentUser={currentUser} allEntries={allEntries}/>} />
         <Route path= "/signup" element={<Signup currentUser={currentUser} setCurrentUser={setCurrentUser} allUsers={allUsers}/>} />
         <Route path= "/login" element={<Login allUsers={allUsers} setCurrentUser={setCurrentUser}/>} />
       </Routes>
--- client/src/components/CreateEvent.js
+++ client/src/components/CreateEvent.js
@@ -2,15 +2,32 @@ import React, {useState, useEffect} from "react";
 import NavBar from "./NavBar"
 
 function CreateEvent() {
+    const [game, setGame] = useState()
+    const [platform, setPlatform]= useState()
+    const [time, setTime] = useState()
+
+    function handleCreate(e) {
+        e.preventDefault()
+        const data = {
+            game: game,
+            platform: platform,
+            time: time
+        }
+        fetch('http://localhost:3000/events',{
+            method: "POST",
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify(data)
+        })
+    }
     return(
         <div>
         <NavBar />
             <div>
                 <form>
-                    <input placeholder="Game"></input>
-                    <input placeholder="Platform"></input>
-                    <input placeholder="Time"></input>
-                    <button>Create Event</button>
+                    <input placeholder="Game" onChange={(e) => setGame(e.target.value)}></input>
+                    <input placeholder="Platform" onChange={(e) => setPlatform(e.target.value)}></input>
+                    <input placeholder="Time" onChange={(e) => setTime(e.target.value)}></input>
+                    <button onClick={(e) => handleCreate(e)}>Create Event</button>
                 </form>
             </div>
         </div>
