d65072315a1857d0009475bb00d423402930feb2
Joshua Schlueter <contact@joshuaschlueter.com>
2022-01-26 15:40:11 -0600

    correction on model>application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  config.middleware.use ActionDispatch::Cookies
-  config.middleware.use ActionDispatch::Session::CookieStore
-
-  config.action_dispatch.cookies_same_site_protection = :strict
 end
