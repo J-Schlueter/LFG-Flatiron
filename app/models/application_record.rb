5bb15dc92c0ddac12f7c6c4f2e9a7db62b29c1c2
Joshua Schlueter <contact@joshuaschlueter.com>
2022-01-26 15:28:14 -0600

    enabled cookies

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use ActionDispatch::Session::CookieStore
+
+  config.action_dispatch.cookies_same_site_protection = :strict
 end
