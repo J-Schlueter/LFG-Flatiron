6b81a727805e506f3bb46b9c627edea79d790056
Joshua Schlueter <contact@joshuaschlueter.com>
2022-03-13 23:55:58 -0500

    landing page hero added

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,7 +5,15 @@ import { Link } from "react-router-dom";
 function LandingPage() {
     return(
         <div>
-            <Link to= "/login" className="landingButton">Login</Link>
+            <div className="landingHero" >
+                <div className="landingHeroText">
+                    <h1>LOOKING FOR GROUP</h1>
+                    <h2>For when you want to group up</h2>
+
+                </div>
+
+            </div>
+            <Link to= "/login" className="landingButtongit ">Login</Link>
             <Link to= "/signup" className="landingButton">Signup</Link>
         </div>
 
